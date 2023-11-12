272402a966ddfffa3473e493b50780cd2a523ec7
djuneyd <djuneyd2009@gmail.com>
2023-11-12 14:51:08 +0400

    virtual_env

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import redirect
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///suggestions.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Offers(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    offer = db.Column(db.Text)
+    def __repr__(self):
+        return f'<Offers{self.id}>' 
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
